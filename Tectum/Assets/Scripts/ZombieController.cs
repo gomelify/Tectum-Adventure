356ae627c7bbd14e42f160bafd8f6127a6a7464e
DerDieDasDavid <d.heitkaemper97@gmx.de>
2016-12-16 00:37:57 +0100

    Simple Zombie AI added

---
+++ Tectum/Assets/Scripts/ZombieController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour {
+
+    public GameObject m_Player;
+    public float m_Speed;
+
+    private Vector3 m_Diff;
+
+	void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+        //Moving towards Player
+        m_Diff = m_Player.transform.position - transform.position;
+        m_Diff.Normalize();
+
+        transform.position = transform.position + (m_Diff * m_Speed);
+
+    }
+}
