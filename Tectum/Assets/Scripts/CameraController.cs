f688e4a672dc796a5282a2196bfb9826801b0a71
gomelify <gomelify@gmail.com>
2016-12-15 20:12:08 +0100

    First

---
+++ Tectum/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject Player;
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        transform.position = Player.transform.position + new Vector3(0,0,-10);
+    }
+}
---
+++ Tectum/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float Speed = 4.5f;
+
+    private Rigidbody2D rb2d;
+    private Vector3 Diff;
+    private float Mouse_X;
+    private float Mouse_Y;
+    private float PlayerPosition;
+    private float Angle;
+
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+
+    void Update()
+    {
+        //Rotation
+        Vector3 Diff = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        Angle = Mathf.Atan2(Diff.y, Diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(Angle, Vector3.forward);
+
+        //Movement
+        Vector2 Movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb2d.MovePosition(rb2d.position + Movement * Time.deltaTime * Speed);
+    }
+}
